c0b2ed1f64bcb4c5da0c19f41d726c005219c3ce
Noy-98 <noy.services01@gmail.com>
2024-05-22 16:29:14 +0800

    functioning part 2

--- php/dashboard/user/home.php
+++ php/dashboard/user/home.php
@@ -1,10 +1,3 @@
-<?php
-session_start();
-if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'admin') {
-  header('Location: ../../login.php');
-  exit();
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
